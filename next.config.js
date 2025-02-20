914f3dba64c967f55ea67746975e57c6016662e9
Andrew Kuan <andrewkuan@Andrews-MacBook-Air.local>
2025-02-20 20:38:47 +0000

    Add environment variables configuration for build

--- next.config.js
+++ next.config.js
@@ -19,4 +19,11 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: false,
   },
+  env: {
+    GITHUB_ID: process.env.GITHUB_ID,
+    GITHUB_SECRET: process.env.GITHUB_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    DATABASE_URL: process.env.DATABASE_URL,
+  },
 } 
